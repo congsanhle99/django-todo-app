e99f40da7d44ffc99a32d32f6e795d6c1fa87bd2
CongSanhLe <congsanhle1999@gmail.com>
2023-08-12 05:57:41 +0700

    [1208-06] mark task done

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from .models import Task
 
@@ -10,4 +10,11 @@ def addTask(request):
     # render(request, "", context)
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect("home")
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_complete = True
+    task.save()
     return redirect("home")
