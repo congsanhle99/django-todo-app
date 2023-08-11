c5b70946e201a15f1ced4f3f1d559f1ef0b45842
CongSanhLe <congsanhle1999@gmail.com>
2023-08-12 05:34:04 +0700

    [1208] add/show complete task

--- todo/views.py
+++ todo/views.py
@@ -4,7 +4,8 @@ from todoApp.models import Task
 
 
 def home(request):
-    tasks = Task.objects.filter(is_complete=False)
-    print(tasks)
-    context = {"tasks": tasks}
+    tasks = Task.objects.filter(is_complete=False).order_by("-update_at")
+    completedTasks = Task.objects.filter(
+        is_complete=True).order_by("-update_at")
+    context = {"tasks": tasks, "completedTasks": completedTasks}
     return render(request, 'home.html', context)
